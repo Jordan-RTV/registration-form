d00fe22cdc1c1879a53cc6ed714ef24409a106f1
Jordan-RTV <158257782+Jordan-RTV@users.noreply.github.com>
2024-10-28 13:03:28 +0800

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    display_name = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
